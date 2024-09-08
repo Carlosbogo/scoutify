4e6fa1d041a9f0730c368acbd57e05596fcf7986
Carlosbogo <bogomcar@gmail.com>
2024-09-08 12:38:52 +0200

    Add browser_config.py

---
+++ src/program/browser_config.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.chrome.options import Options
+
+config = Options()
+
+language = "en"
+
+config.add_argument(f"--lang={language}")
+config.add_argument("--disable-search-engine-choice-screen")
