48bdac3b303bdc5a44b7674fdf16f940b8204959
Carlosbogo <bogomcar@gmail.com>
2024-09-08 12:40:35 +0200

    Add __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,35 @@
+
+import logging
+import time
+import os
+
+import program.helpers.popups as popups
+from program.ratings import get_rating
+from data.parse_data import parse
+from program.driver import driver
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+ratings = {}
+companies = parse(os.path.join(os.getcwd(), "data\\remote_companies.csv"))
+
+
+try:
+    popups.reject_google_privacy_popup(driver, logger)
+    driver.implicitly_wait(10)
+    for c in companies:
+        ratings.update(get_rating(driver, c[0]))
+    
+except Exception as e:
+    print(repr(e))
+    print(e)
+    time.sleep(2)
+
+finally:
+    driver.quit()
+
+print("finished")
