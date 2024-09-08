f04952be2984381a59d079bd50f2dac84db909d4
Carlosbogo <bogomcar@gmail.com>
2024-09-08 12:38:20 +0200

    Add helpers/popups.py

---
+++ src/program/helpers/popups.py
@@ -0,0 +1,18 @@
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def reject_google_privacy_popup(
+    driver: webdriver, logger: logging.Logger = None, reject: bool = True
+) -> None:
+    deny_cookies_button = driver.find_element(
+        By.XPATH, '//button[.//div[contains(text(), "Rechazar todo")]]'
+    )
+    driver.implicitly_wait(10)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(deny_cookies_button)
+    ).click()
+    logger.info("Google Privacy Popup rejected.")
