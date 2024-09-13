1afe2d70ffdfdd7e9e0e7d16bcc2e8648edfab01
Carlosbogo <bogomcar@gmail.com>
2024-09-13 23:25:17 +0200

    Use local_development env var logic to avoid google cookies popup errors

--- src/app.py
+++ src/app.py
@@ -13,9 +13,15 @@ ratings = {}
 companies = parse(os.path.join(os.getcwd(), "data\\remote_companies.csv"))
 
 
+if const.LOCAL_DEVELOPMENT:
+    try:
+        popups.reject_google_privacy_popup(driver, logger)
+        logger.info("Google privacy popup rejected.")
+    except Exception as e:
+        logger.error("Error rejecting Google privacy popup.")
+
 try:
-    popups.reject_google_privacy_popup(driver, logger)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     for c in companies:
         ratings.update(get_rating(driver, c[0]))
     
