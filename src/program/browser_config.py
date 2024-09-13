73e2d918fbedd7d969ff1655fd3ff89ee00abcd7
Carlosbogo <bogomcar@gmail.com>
2024-09-13 23:34:22 +0200

    Update chrome driver language

--- src/program/browser_config.py
+++ src/program/browser_config.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.options import Options
 
 config = Options()
 
-language = "en"
+language = "en-US"
 
 config.add_argument(f"--lang={language}")
 config.add_argument("--disable-search-engine-choice-screen")
