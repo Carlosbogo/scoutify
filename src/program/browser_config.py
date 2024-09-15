d90b65b9b7d10562f1cf5ad6af404bc7cb559ac5
Carlosbogo <bogomcar@gmail.com>
2024-09-15 19:25:26 +0200

    Add arguments to browser config

--- src/program/browser_config.py
+++ src/program/browser_config.py
@@ -7,5 +7,7 @@ language = "en-US"
 config.add_argument(f"--lang={language}")
 config.add_argument("--disable-search-engine-choice-screen")
 config.add_argument("--headless=new")
+config.add_argument("-width=1920")
+config.add_argument("-height=1080")
 config.add_argument("--no-sandbox")
 config.add_argument("--disable-gpu")
