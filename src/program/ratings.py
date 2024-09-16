fbce762b28847a8ece861a5918f0cbcbafae9743
Carlosbogo <bogomcar@gmail.com>
2024-09-16 21:24:09 +0200

    Refactor ratings.py

--- src/program/ratings.py
+++ src/program/ratings.py
@@ -3,26 +3,42 @@ import os
 from selenium.webdriver.common.by import By
 from program.helpers.logging import logger
 
+
+def get_text(driver, text, max_attempts=5):
+    attempt = 1
+    xpath = f'//span[text()[contains(., "{text}")]]'
+    value = 0
+    while attempt <= max_attempts:
+        try:
+            value = driver.find_element(By.XPATH, xpath).text
+            break
+        except:
+            logger.error(f"Error getting value. Attempt {attempt} of {max_attempts}")
+            time.sleep(2)
+            attempt += 1
+    return value
+
+
 def get_rating(driver, company):
     logger.info(f"Getting rating for {company}")
     search = f"https://www.google.com/search?q={company}+glassdoor"
     driver.get(search)
 
-    attempt = 1
-    max_attempts = 5
-    rating_msg = None
-    while attempt <= max_attempts:
-        try:
-            rating_msg = driver.find_element(By.XPATH, '//span[text()[contains(., "Rating")]]').text
-            break
-        except:
-            logger.error(f"Error getting rating. Attempt {attempt} of {max_attempts}")
-            time.sleep(2)
-            attempt += 1
-    if attempt == 5:
-        logger.error(f"Could not get rating for {company}. Skipping...")
-        return [company, 0]
-    else:
+    rating_msg = get_text(driver, "Rating")
+    votes_msg = get_text(driver, "vote")
+
+    if rating_msg:
+        # We replace commas with dots in case the locale uses commas as
+        # decimal separator (could be useful for local testing)
         rating = float(rating_msg.split(" ")[1].replace(",", "."))
-        logger.info(f"Rating for {company}: {rating_msg}")
-        return [company, rating]
+    else:
+        rating = 0
+
+    if votes_msg:
+        # We remove commas in case the value exceeds 1,000
+        votes = int(votes_msg.split(" ")[0].replace(",", ""))
+    else:
+        votes = 0
+
+    logger.info(f"Rating for {company}: {rating} with {votes} votes.")
+    return [company, rating, votes]
