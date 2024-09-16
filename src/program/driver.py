b9d4505cdc694187d268166202c1ac4c5c97ebde
Carlosbogo <bogomcar@gmail.com>
2024-09-16 21:45:11 +0200

    Update implicit wait for selenium chrome webdriver

--- src/program/driver.py
+++ src/program/driver.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 from program.browser_config import config
 
 driver = webdriver.Chrome(options=config)
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
 driver.get("https://www.google.com")
