b51780d669b35a3380ce732a71d10409f0b5a584
Carlosbogo <bogomcar@gmail.com>
2024-09-20 23:54:31 +0200

    Create driver.py

---
+++ services/glassdoor_scraper/src/driver.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+config = Options()
+
+language = "en-US"
+
+config.add_argument(f"--lang={language}")
+config.add_argument("--disable-search-engine-choice-screen")
+config.add_argument("--headless=new")
+config.add_argument("-width=1920")
+config.add_argument("-height=1080")
+config.add_argument("--no-sandbox")
+config.add_argument("--disable-gpu")
+
+driver = webdriver.Chrome(options=config)
+driver.implicitly_wait(10)
+driver.get("https://www.google.com")
