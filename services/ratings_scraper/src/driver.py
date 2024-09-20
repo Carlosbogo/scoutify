1b98d2b5549dd71d67460b2c79cc44ff22b3f449
Carlosbogo <bogomcar@gmail.com>
2024-09-20 23:55:44 +0200

    Move browser config into driver.py

--- services/ratings_scraper/src/driver.py
+++ services/ratings_scraper/src/driver.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
-from src.browser_config import config
+from selenium.webdriver.chrome.options import Options
+
+config = Options()
+
+language = "en-US"
+
+config.add_argument(f"--lang={language}")
+config.add_argument("--disable-search-engine-choice-screen")
+config.add_argument("--headless=new")
+config.add_argument("-width=1920")
+config.add_argument("-height=1080")
+config.add_argument("--no-sandbox")
+config.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=config)
 driver.implicitly_wait(10)
