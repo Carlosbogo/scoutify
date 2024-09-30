707e543165b821ae877ea9a097d5b1c8310df60f
Carlosbogo <bogomcar@gmail.com>
2024-09-30 20:38:45 +0200

    Complete constants.py

--- services/glassdoor_scraper/constants.py
+++ services/glassdoor_scraper/constants.py
@@ -1,1 +1,13 @@
-GOOGLE_CACHE_PREFIX = "http://webcache.googleusercontent.com/search?q=cache:"
+import os
+import dotenv
+
+dotenv.load_dotenv(".env")
+
+GCP_PROJECT = os.getenv("GCP_PROJECT")
+COMPANIES_BUCKET_NAME = os.getenv("COMPANIES_BUCKET_NAME")
+
+STARTING_PAGE_NUMBER = os.getenv("STARTING_PAGE_NUMBER", 1)
+MIN_RATING = os.getenv("MIN_RATING", 3.5)
+
+CSV_PATH = os.getenv("CSV_PATH", "companies.csv")
+CSV_HEADER = ["Company", "Rating", "Reviews", "Locations"]
