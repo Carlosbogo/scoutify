777d0d3e63b2d89d5ed7be594ad2324962777096
Carlosbogo <bogomcar@gmail.com>
2024-09-30 20:32:10 +0200

    Switch drivers to seleniumbase

--- services/glassdoor_scraper/src/driver.py
+++ services/glassdoor_scraper/src/driver.py
@@ -1,20 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-config = Options()
-
-language = "en-US"
-
-config.add_argument(f"--lang={language}")
-config.add_argument("--disable-search-engine-choice-screen")
-config.add_argument("--headless=new")
-config.add_argument("-width=1920")
-config.add_argument("-height=1080")
-config.add_argument("--no-sandbox")
-config.add_argument("--disable-gpu")
+from seleniumbase import Driver
 
 
-def get_driver():
-    driver = webdriver.Chrome(options=config)
+def get_driver() -> Driver:
+    driver = Driver(browser="chrome", uc=True, incognito=True, headless=True)
     driver.implicitly_wait(10)
     return driver
