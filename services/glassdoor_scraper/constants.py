362991a4499c14570c3bc69c5ec50bced2aeaea9
Carlosbogo <bogomcar@gmail.com>
2024-10-12 16:12:28 +0200

    Fix STARTING_PAGE_NUMBER env var dtype

--- services/glassdoor_scraper/constants.py
+++ services/glassdoor_scraper/constants.py
@@ -6,7 +6,7 @@ dotenv.load_dotenv(".env")
 GCP_PROJECT = os.getenv("GCP_PROJECT")
 COMPANIES_BUCKET_NAME = os.getenv("COMPANIES_BUCKET_NAME")
 
-STARTING_PAGE_NUMBER = os.getenv("STARTING_PAGE_NUMBER", 1)
+STARTING_PAGE_NUMBER = int(os.getenv("STARTING_PAGE_NUMBER", 1))
 MIN_RATING = os.getenv("MIN_RATING", 3.5)
 
 CSV_PATH = os.getenv("CSV_PATH", "companies.csv")
