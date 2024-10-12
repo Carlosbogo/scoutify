c4836cfbfe86be388d375d00ff0daf6db5662d4e
Carlosbogo <bogomcar@gmail.com>
2024-10-12 16:11:18 +0200

    Refactor glassdoor_scraper logic for better failure recovery

--- services/glassdoor_scraper/app.py
+++ services/glassdoor_scraper/app.py
@@ -20,36 +20,33 @@ current_page = const.STARTING_PAGE_NUMBER
 driver.get(f"https://www.glassdoor.es/Opiniones/index.htm?overall_rating_low={const.MIN_RATING}&page={current_page}")
 handle_glassdoor_cookies_popup(driver)
 
-try:
-    while True:
-        logger.info(f"Scraping page {current_page}...")
-        company_elements = get_companies_in_page(driver)
-        companies = []
-        for element in company_elements:
-            companies.append(get_company_info(driver, element))
 
-        logger.info(f"Page {current_page} scraped")
+while True:
+    logger.info(f"Scraping page {current_page}...")
+    company_elements = get_companies_in_page(driver)
+    companies = []
+    for element in company_elements:
+        companies.append(get_company_info(driver, element))
 
-        data.write_csv(companies, const.CSV_PATH, const.CSV_HEADER, ";")
+    logger.info(f"Page {current_page} scraped")
+    data.write_csv(companies, const.CSV_PATH, const.CSV_HEADER, ";")
 
-        if current_page % 30 == 0:
-            data.upload_to_bucket(
-                const.COMPANIES_BUCKET_NAME,
-                "companies.csv",
-                f"companies-rating-{const.MIN_RATING}-{const.STARTING_PAGE_NUMBER}-{current_page}-{execution_id}.csv",
-                const.GCP_PROJECT
-            )
-            logger.info("Data uploaded to bucket")
-
+    if current_page % 30 == 0:
+        data.upload_to_bucket(
+            const.COMPANIES_BUCKET_NAME,
+            "companies.csv",
+            f"{execution_id}-{const.MIN_RATING}/companies-rating-{const.STARTING_PAGE_NUMBER}-{current_page}.csv",
+            const.GCP_PROJECT
+        )
+        logger.info("Data uploaded to bucket")
+    try:
         click_next_page(driver)
-        current_page += 1
+    except Exception as e:
+        logger.error(e)
+        break
+    current_page += 1
 
-
-except Exception as e:
-    logger.error(e)
-
-finally:
-    driver.quit()
+driver.quit()
 
 logger.info(f"Scraper finished. Data saved to {const.CSV_PATH}")
 logger.info(f"Last page scraped: {current_page}")
